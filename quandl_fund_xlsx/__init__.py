02561a7103fdda948eb47c8bdf534fdb267a2c63
robren <rob@robren.net>
2017-11-06 09:37:48 -0500

    Bump version: 0.1.3 → 0.1.4

--- quandl_fund_xlsx/__init__.py
+++ quandl_fund_xlsx/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Robert Rennison"""
 __email__ = 'rob@robren.net'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- quandl_fund_xlsx/cli.py
+++ quandl_fund_xlsx/cli.py
@@ -27,7 +27,7 @@ from .fundamentals import stock_xlsx
 
 
 def main(args=None):
-    arguments = docopt(__doc__, version='0.1.3')
+    arguments = docopt(__doc__, version='0.1.4')
     print(arguments)
 
     file = arguments['--input']
