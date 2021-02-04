1e97d6341064238f41324b0782d46bda6d6a03fe
Rob Rennison <rob@robren.net>
2021-02-04 17:01:59 -0500

    missed a comma when updating the classifiers

--- setup.py
+++ setup.py
@@ -3,58 +3,53 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'docopt>=0.6.0',
-    'pandas',
-    'quandl',
-    'xlsxwriter',
-    'pathlib',
-    'requests>=2.20.0'
+    "docopt>=0.6.0",
+    "pandas",
+    "quandl",
+    "xlsxwriter",
+    "pathlib",
+    "requests>=2.20.0",
 ]
 
 test_requirements = [
-    'pytest',
+    "pytest",
 ]
 
 setup(
-    name='quandl_fund_xlsx',
-    version='0.4.1',
+    name="quandl_fund_xlsx",
+    version="0.4.1",
     description="A CLI tool using the Quandl API and the Sharadar Fundamentals database.",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author="Robert Rennison",
-    author_email='rob@robren.net',
-    url='https://github.com/robren/quandl_fund_xlsx',
+    author_email="rob@robren.net",
+    url="https://github.com/robren/quandl_fund_xlsx",
     packages=[
-        'quandl_fund_xlsx',
+        "quandl_fund_xlsx",
     ],
-    package_dir={'quandl_fund_xlsx':
-                 'quandl_fund_xlsx'},
-    entry_points={
-        'console_scripts': [
-            'quandl_fund_xlsx=quandl_fund_xlsx.cli:main'
-        ]
-    },
+    package_dir={"quandl_fund_xlsx": "quandl_fund_xlsx"},
+    entry_points={"console_scripts": ["quandl_fund_xlsx=quandl_fund_xlsx.cli:main"]},
     include_package_data=True,
     install_requires=requirements,
     license="Apache Software License 2.0",
     zip_safe=False,
-    keywords='quandl_fund_xlsx quandl finance ratios',
+    keywords="quandl_fund_xlsx quandl finance ratios",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-        'Programming Language :: Python :: 3.9'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    test_suite="tests",
+    tests_require=test_requirements,
 )
